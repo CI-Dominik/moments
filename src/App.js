a2f14f271a17e6aad68aa6b432db397c91125d9c
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-02-13 11:41:09 +0100

    Add routing to navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
+import Container from 'react-bootstrap/container'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/sign-in" render={() => <h1>Sign-in</h1>} />
+          <Route exact path="/sign-up" render={() => <h1>Sign-up</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,27 +2,30 @@ import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
+import { NavLink } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
+                <NavLink to="/">
                 <Navbar.Brand><img src={logo} alt="Logo" height="45" /></Navbar.Brand>
+                </NavLink>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse>
                     <Nav
                         className="ms-auto text-center"
                         style={{ maxHeight: '100px' }}
                     >
-                        <Nav.Link>
+                        <NavLink to="/">
                             <i className="fas fa-home"></i> Home
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink to="/sign-in">
                             <i className="fas fa-sign-in-alt"></i> Sign in
-                        </Nav.Link>
-                        <Nav.Link>
+                        </NavLink>
+                        <NavLink to="/sign-up">
                             <i className="fas fa-user-plus"></i> Sign up
-                        </Nav.Link>
+                        </NavLink>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
