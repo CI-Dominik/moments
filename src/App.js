b5d5e49b2b0547147bd5ab39bb83ebefb0ddac3a
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-02-13 11:45:49 +0100

    Update package-lock and correct typo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import Container from 'react-bootstrap/container'
+import Container from 'react-bootstrap/Container'
 import { Route, Switch } from 'react-router-dom'
 
 function App() {
