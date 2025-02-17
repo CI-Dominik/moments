082e76f179435a634b3cfb91cebfb9885f0f6114
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-02-17 11:50:01 +0100

    Add sign in and adjust NavBar when logged in

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/logo.png'
 import styles from '../styles/NavBar.module.css'
 import { NavLink } from 'react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext);
+    const loggedInIcons = (
+        <>
+        {currentUser?.username}
+        </>
+    );
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/sign-in">
+                <i className="fas fa-sign-in-alt"></i> Sign-in
+            </NavLink>
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/sign-up">
+                <i className="fas fa-user-plus"></i> Sign-up
+            </NavLink>
+        </>
+    );
+
     return (
         <Navbar className={styles.NavBar} expand="md" fixed="top">
             <Container>
@@ -24,20 +50,8 @@ const NavBar = () => {
                             to="/">
                             <i className="fas fa-home"></i> Home
                         </NavLink>
-                        <NavLink
-                            exact
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/sign-in">
-                            <i className="fas fa-sign-in-alt"></i> Sign-in
-                        </NavLink>
-                        <NavLink
-                            exact
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/sign-up">
-                            <i className="fas fa-user-plus"></i> Sign-up
-                        </NavLink>
+                        {currentUser ? loggedInIcons : loggedOutIcons}
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
