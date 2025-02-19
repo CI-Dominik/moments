8aa63d5ed3a0447a4f4aae97a635223cb069df33
Dominik Wulf <Mail@Dominik-Wulf.de>
2025-02-19 17:43:57 +0100

    Update CurrentUserContext and posts page

--- src/contexts/CurrentUserContext.js
+++ src/contexts/CurrentUserContext.js
@@ -27,46 +27,46 @@ export const CurrentUserProvider = ({ children }) => {
     }, []);
 
     useMemo(() => {
-        axiosReq.interceptors.response.use(
-            async (config) => {
-                try {
-                    await axios.post('/dj-rest-auth/token/refresh/')
-                } catch (error) {
-                    setCurrentUser((prevCurrentUser) => {
-                        if (prevCurrentUser) {
-                            history.push('/sign-in')
-                        }
-                        return null;
-                    })
-                    return config;
+        axiosReq.interceptors.request.use(
+          async (config) => {
+            try {
+              await axios.post("/dj-rest-auth/token/refresh/");
+            } catch (error) {
+              setCurrentUser((prevCurrentUser) => {
+                if (prevCurrentUser) {
+                  history.push("/sign-in");
                 }
-                return config;
-            },
-            (error) => {
-                return Promise.reject(error);
+                return null;
+              });
+              return config;
             }
-        )
-
+            return config;
+          },
+          (error) => {
+            return Promise.reject(error);
+          }
+        );
+    
         axiosRes.interceptors.response.use(
-            (response) => response,
-            async (error) => {
-                if (error.response?.status === 401) {
-                    try {
-                        await axios.post('dj-rest-auth/token/refresh/')
-                    } catch {
-                        setCurrentUser(prevCurrentUser => {
-                            if (prevCurrentUser) {
-                                history.push('/sign-in')
-                            }
-                            return null;
-                        })
-                    }
-                    return axios(error.config)
-                }
-                return Promise.reject(error)
+          (response) => response,
+          async (error) => {
+            if (error.response?.status === 401) {
+              try {
+                await axios.post("/dj-rest-auth/token/refresh/");
+              } catch (error) {
+                setCurrentUser((prevCurrentUser) => {
+                  if (prevCurrentUser) {
+                    history.push("/sign-in");
+                  }
+                  return null;
+                });
+              }
+              return axios(error.config);
             }
-        )
-    }, [history])
+            return Promise.reject(error);
+          }
+        );
+      }, [history]);
 
     return (
         <CurrentUserContext.Provider value={currentUser}>
